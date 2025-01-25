e517d5d76c431fbad7aca5dcc2af6c37e0b21f11
Harshana Wijerathne <harshanawijerathne08@gmail.com>
2025-01-25 19:00:46 +0530

    Accept the connection

--- src/main/java/lk/athenaWebServer/ServerApp.java
+++ src/main/java/lk/athenaWebServer/ServerApp.java
@@ -2,11 +2,18 @@ package lk.athenaWebServer;
 
 import java.io.IOException;
 import java.net.ServerSocket;
+import java.net.Socket;
 
 public class ServerApp {
     public static void main(String[] args) throws Exception {
         ServerSocket serverSocket = new ServerSocket(80);
         System.out.println("Server started on port 80");
         System.out.println("Waiting for connections...");
+
+        while (true) {
+            Socket localsocket = serverSocket.accept();
+            System.out.println("Accepted connection from " + localsocket.getRemoteSocketAddress());
+
+        }
     }
 }
