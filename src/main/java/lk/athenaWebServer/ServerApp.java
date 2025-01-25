7ee9640d78aa607fe41b81ad25f95b350429138f
Harshana Wijerathne <harshanawijerathne08@gmail.com>
2025-01-25 19:26:41 +0530

    check weather http protocol receive succesfully

--- src/main/java/lk/athenaWebServer/ServerApp.java
+++ src/main/java/lk/athenaWebServer/ServerApp.java
@@ -1,8 +1,12 @@
 package lk.athenaWebServer;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.security.spec.RSAOtherPrimeInfo;
 
 public class ServerApp {
     public static void main(String[] args) throws Exception {
@@ -13,6 +17,31 @@ public class ServerApp {
         while (true) {
             Socket localsocket = serverSocket.accept();
             System.out.println("Accepted connection from " + localsocket.getRemoteSocketAddress());
+
+            new Thread(() -> {
+                try {
+                    InputStream inputStream = localsocket.getInputStream();
+                    InputStreamReader isr = new InputStreamReader(inputStream);
+                    BufferedReader br = new BufferedReader(isr);
+
+                    String commandLine = br.readLine();
+                    String[] s = commandLine.split(" ");
+                    String command = s[0];
+                    String path = s[1];
+                    System.out.println(command + " " + path);
+
+                    String line;
+                    String host;
+                    while ((line = br.readLine())!= null && !line.isEmpty()){
+                        if(line.split(":")[0].strip().equalsIgnoreCase("host")){
+                            host = line.split(":")[1];
+                            System.out.println(host);
+                        }
+                    }
+                } catch (Exception e) {
+                    throw new RuntimeException(e);
+                }
+            }).start();
 
         }
     }
